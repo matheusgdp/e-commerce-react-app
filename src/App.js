0c1c833f19c1e199eaac439d94818bcc7ccea75a
matheusgdp <matheusdepaul@hotmail.com>
2020-05-25 14:59:44 -0300

    Updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-)
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
+    // Com o Switch no momento em que a url da um match com um path, ele não continua renderizando o resto.
     <div>
         <Switch>
             <Route exact path='/' component={HomePage}/>
-            <Route path='/hats' component={HatsPage}/>
+            <Route path='/shop' component={ShopPage}/>
         </Switch>
     </div>
   )
